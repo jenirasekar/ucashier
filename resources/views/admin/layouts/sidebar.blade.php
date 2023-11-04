857a01d924fc93e0921856ad4b717c4d59ba3925
Jenira Sekar Az-zahra <jenirasekar@gmail.com>
2023-11-04 11:34:00 +0700

    refactor: aksesibilitas untuk role

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -14,12 +14,12 @@
                 <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
                 <li class="nav-item">
-                    <a href="/" class="nav-link">
+                    <a href="/admin/dashboard" class="nav-link">
                         <i class="nav-icon fas fa-th"></i>
                         <p>Dashboard</p>
                     </a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item" style="display: {{ auth()->user()->role != 'admin' ? 'none' : 'block' }}">
                     <a href="/admin/user" class="nav-link">
                         <i class="nav-icon fas fa-users"></i>
                         <p>User</p>
