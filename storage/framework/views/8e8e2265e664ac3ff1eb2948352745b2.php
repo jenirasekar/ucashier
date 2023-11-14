a439eee20c9f947f6bea0ce72d7102033b41970f
Jenira Sekar Az-zahra <jenirasekar@gmail.com>
2023-11-14 11:38:38 +0700

    .gitignore is now working

--- storage/framework/views/8e8e2265e664ac3ff1eb2948352745b2.php
+++
@@ -1,4 +0,0 @@
-<?php echo $__env->make('sweetalert::alert', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-<?php if($content): ?>
-    <?php echo $__env->make($content, \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-<?php endif; ?><?php /**PATH E:\ucashier\resources\views/admin/layouts/content.blade.php ENDPATH**/ ?>
--- storage/framework/views/e73c4e63ae91929b29109bacec83c96d.php
+++
@@ -1,6 +0,0 @@
-
-
-<div class="container-fluid">
-    <h1>Selamat datang</h1> 
-    <p><?php echo e(auth()->user()->name); ?>, Anda login sebagai <span style="text-transform: capitalize"><?php echo e(auth()->user()->role); ?></span></p>
-</div><?php /**PATH E:\ucashier\resources\views/admin/dashboard/index.blade.php ENDPATH**/ ?>
