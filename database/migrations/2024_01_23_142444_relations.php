f3e82cc1d012fe3ef38257607adeb4770dc481cf
Jenira Sekar Az-zahra <jenirasekar@gmail.com>
2024-01-24 12:56:01 +0700

    chore: membuat relasi antar table

---
+++ database/migrations/2024_01_23_142444_relations.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('transaksis', function (Blueprint $table) {
+            $table->foreign('user_id')->references('id')->on('users');
+            
+            $table->unsignedBigInteger('pelanggan_id');
+            $table->foreign('pelanggan_id')->references('id')->on('pelanggans');
+        });
+
+        Schema::table('transaksi_details', function (Blueprint $table) {
+            $table->foreign('produk_id')->references('id')->on('produks');
+        });
+
+
+        Schema::table('produks', function (Blueprint $table) {
+            $table->foreign('kategori_id')->references('id')->on('kategoris');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
