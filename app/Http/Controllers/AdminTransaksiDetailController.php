bad5c4730b71b8245dccc79b9b3cf59f5d496757
Jenira Sekar Az-zahra <jenirasekar@gmail.com>
2024-03-01 07:31:26 +0700

    Revert "refactor: null transaksi_id"
    This reverts commit dc7e4cdff582c8a80600abf5e64472ba1455e729.

--- app/Http/Controllers/AdminTransaksiDetailController.php
+++ app/Http/Controllers/AdminTransaksiDetailController.php
@@ -18,9 +18,9 @@ class AdminTransaksiDetailController extends Controller
         $id_transaksi = $request->transaksi_id;
 
         $transaksi = Transaksi::find($id_transaksi);
-        $transaksi = Transaksi::where('status', 'pending')->first();
 
         if ($transaksi == null) {
+            $transaksi = Transaksi::where('status', 'pending')->first();
             $transaksi = Transaksi::create([
                 'user_id' => auth()->user()->id,
                 'total' => 0,
--- resources/views/transaksi/create.blade.php
+++ resources/views/transaksi/create.blade.php
@@ -3,7 +3,7 @@
         <form action="{{ route('detailtransaksi.store') }}" method="post" id="form_detail_transaksi">
             @csrf
             <input type="hidden" name="produk_name" id="nama_produk" value="">
-            <input type="hidden" name="transaksi_id" id="transaksi_id" value="1">
+            <input type="hidden" name="transaksi_id" id="id_transaksi" value="">
             <input type="hidden" name="subtotal" id="subtotal">
             <div class="form-group row">
                 <div class="col-2">
@@ -105,6 +105,8 @@
             </div>
             <form action="{{ route('transaksi.store') }}" method="post" id="form_transaksi">
                 @csrf
+                <input type="hidden" name="transaksi_id" id="transaksi_id">
+                <input type="hidden" name="transaksi_id" id="transaksi_id2">
                 <div class="modal-body">
                     <div class="mb-2">
                         <label for="total">Total</label>
@@ -181,6 +183,7 @@
                     // total dalam modal
                     $('#total-belanja').val(total);
                     $('#transaksi_id').val(response.transaksi_id);
+                    $('#transaksi_id2').val(response.transaksi_id);
                 }
             });
         });
@@ -241,7 +244,7 @@
                         $('#total-belanja').val($('#total').val());
 
                         $('#nama_produk').val(response.produk_name);
-                        $('#transaksi_id').val(response.transaksi_id);
+                        $('#id_transaksi').val(response.transaksi_id);
 
                         // clear input fields
                         $('#id_produk, #harga, #qty, #subtotal').val('');
